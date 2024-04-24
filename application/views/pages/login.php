07a4e3d371275018158c3a6f7558c35d64c7d695
NyTendry <146230318+NyTendry@users.noreply.github.com>
2024-04-24 12:01:08 +0300

    Update login.php

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<!-- Coding By CodingNepal - codingnepalweb.com -->
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -14,10 +13,11 @@
     <div id="loginForm" class="login form"> <!-- Added ID 'loginForm' here -->
       <header>Login</header>
       <form action="#" method="GET">
-        <input type="text" name="pseudo" placeholder="Enter your email"> <!-- Added ID 'loginEmail' here -->
-        <input type="password" name="mdp" placeholder="Enter your password"> <!-- Added ID 'loginPassword' here -->
+        <input type="text" name="pseudo" placeholder="Enter your email" required> <!-- Added ID 'loginEmail' here -->
+        <input type="password" name="mdp" placeholder="Enter your password" required> <!-- Added ID 'loginPassword' here -->
         <a href="#">Forgot password?</a>
         <input type="submit" class="button" value="Login">
+        <div id="errorDiv" style="color: red;"></div>
       </form>
       <div class="signup">
         <span class="signup">Don't have an account?
@@ -28,9 +28,9 @@
     <div class="registration form">
       <header>Signup</header>
       <form action="#">
-        <input type="text" name="email" placeholder="Enter your email"> <!-- Added ID 'signupEmail' here -->
-        <input type="password" name="mdp" placeholder="Create a password"> <!-- Added ID 'signupPassword' here -->
-        <input type="password" name="mdp" placeholder="Confirm your password"> <!-- Added ID 'confirmPassword' here -->
+        <input type="text" name="email" placeholder="Enter your email" required> <!-- Added ID 'signupEmail' here -->
+        <input type="password" name="mdp" placeholder="Create a password" required> <!-- Added ID 'signupPassword' here -->
+        <input type="password" name="mdp" placeholder="Confirm your password" required> <!-- Added ID 'confirmPassword' here -->
         <input type="submit" class="button" value="Signup">
       </form>
       <div class="signup">
@@ -40,5 +40,7 @@
       </div>
     </div>
   </div>
+      <!-- Error message div -->
+      <div id="errorDiv" style="color: red;"></div>
 </body>
 </html>
