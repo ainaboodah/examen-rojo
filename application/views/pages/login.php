5eedd99bae50e936744573f4f9383e5afe05bd7e
NyTendry <146230318+NyTendry@users.noreply.github.com>
2024-04-24 10:53:33 +0300

    Update login.php

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Login & Registration Form</title>
-  <link rel="stylesheet" href="<?php echo base_url('assets/styles.css');?>">
+  <link rel="stylesheet" href="<?php echo base_url('assets/login.css');?>">
 </head>
 <body>
   <div class="container">
